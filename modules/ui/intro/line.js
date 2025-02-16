tom-konda/iD

commit 7559dead80f518fc41692bc0aec2bb534162a2af

add missing retries in walkthrough if edit menu is closed accidentally
e.g. for the "delete node" step, when the map is accidentally panned the edit menu will be closed, causing the walkthrough to get stuck

modules/ui/intro/line.js
@@ -1000,6 +1000,9 @@ export function uiIntroLine(context, reveal) {
         );
 
         context.map().on('move.intro drawn.intro', function() {
+            if (selectMenuItem(context, 'delete').empty()) {
+                return continueTo(multiRightClick);
+            }
             reveal('.edit-menu',
                 helpHtml('intro.lines.multi_delete'),
                 { duration: 0, padding: 50 }

modules/ui/intro/point.js
@@ -429,6 +429,9 @@ export function uiIntroPoint(context, reveal) {
 
         timeout(function() {
             context.map().on('move.intro', function() {
+                if (selectMenuItem(context, 'delete').empty()) {
+                    return continueTo(rightClickPoint);
+                }
                 reveal('.edit-menu',
                     helpHtml('intro.points.delete'),
                     { duration: 0,  padding: 50 }

commit 9b7b0cde9e5d25c5a57da58c381cf230af1098cb

fix walkthrough from not correctly registering deleted ways
fixes #10776, this was a regression in 7559dea

modules/ui/intro/line.js
@@ -1000,9 +1000,6 @@ export function uiIntroLine(context, reveal) {
         );
 
         context.map().on('move.intro drawn.intro', function() {
-            if (selectMenuItem(context, 'delete').empty()) {
-                return continueTo(multiRightClick);
-            }
             reveal('.edit-menu',
                 helpHtml('intro.lines.multi_delete'),
                 { duration: 0, padding: 50 }