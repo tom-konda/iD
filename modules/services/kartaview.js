tom-konda/iD

commit 19957d0e39426f52bfa81a117be92ffe69fc09ee

keep photo viewer state on reset, properly clean up active marker
when switching between photos from different providers

fixes #10987

modules/services/kartaview.js
@@ -7,6 +7,7 @@ import RBush from 'rbush';
 import { localizer } from '../core/localizer';
 import { geoExtent, geoScaleToZoom } from '../geo';
 import { utilQsString, utilRebind, utilSetTransform, utilStringQs, utilTiler } from '../util';
+import { services } from './';
 
 
 var apibase = 'https://kartaview.org';
@@ -191,8 +192,6 @@ export default {
             images: { inflight: {}, loaded: {}, nextPage: {}, rtree: new RBush(), forImageKey: {} },
             sequences: {}
         };
-
-        _oscSelectedImage = null;
     },
 
 
@@ -368,21 +367,22 @@ export default {
 
 
     showViewer: function(context) {
-        var viewer = context.container().select('.photoviewer')
-            .classed('hide', false);
-
+        var viewer = context.container().select('.photoviewer');
         var isHidden = viewer.selectAll('.photo-wrapper.kartaview-wrapper.hide').size();
 
         if (isHidden) {
-            viewer
-                .selectAll('.photo-wrapper:not(.kartaview-wrapper)')
-                .classed('hide', true);
-
+            for (const service of Object.values(services)) {
+                if (service === this) continue;
+                if (typeof service.hideViewer === 'function') {
+                    service.hideViewer(context);
+                }
+            }
             viewer
                 .selectAll('.photo-wrapper.kartaview-wrapper')
                 .classed('hide', false);
         }
 
+        viewer.classed('hide', false);
         return this;
     },
 

modules/services/mapilio.js
@@ -10,6 +10,7 @@ import { isEqual } from 'lodash';
 import { utilRebind, utilTiler, utilQsString, utilStringQs, utilSetTransform } from '../util';
 import {geoExtent, geoScaleToZoom} from '../geo';
 import {localizer} from '../core/localizer';
+import { services } from './';
 
 const apiUrl = 'https://end.mapilio.com';
 const imageBaseUrl = 'https://cdn.mapilio.com/im';
@@ -222,8 +223,6 @@ export default {
             sequences: { rtree: new RBush(), lineString: {} },
             requests: { loaded: {}, inflight: {} }
         };
-
-        _activeImage = null;
     },
 
     // Get visible images
@@ -575,21 +574,22 @@ export default {
     },
 
     showViewer:function (context) {
-        let wrap = context.container().select('.photoviewer')
-            .classed('hide', false);
-
+        let wrap = context.container().select('.photoviewer');
         let isHidden = wrap.selectAll('.photo-wrapper.mapilio-wrapper.hide').size();
 
         if (isHidden) {
-            wrap
-                .selectAll('.photo-wrapper:not(.mapilio-wrapper)')
-                .classed('hide', true);
-
+            for (const service of Object.values(services)) {
+                if (service === this) continue;
+                if (typeof service.hideViewer === 'function') {
+                    service.hideViewer(context);
+                }
+            }
             wrap
                 .selectAll('.photo-wrapper.mapilio-wrapper')
                 .classed('hide', false);
         }
 
+        wrap.classed('hide', false);
         return this;
     },
 

modules/services/mapillary.js
@@ -8,6 +8,7 @@ import { VectorTile } from '@mapbox/vector-tile';
 
 import { geoExtent, geoScaleToZoom } from '../geo';
 import { utilQsString, utilRebind, utilTiler, utilStringQs } from '../util';
+import { services } from './';
 
 const accessToken = 'MLY|4100327730013843|5bb78b81720791946a9a7b956c57b7cf';
 const apiUrl = 'https://graph.mapillary.com/';
@@ -260,8 +261,6 @@ export default {
             sequences: { rtree: new RBush(), lineString: {} },
             requests: { loaded: {}, inflight: {} }
         };
-
-        _mlyActiveImage = null;
     },
 
     // Get visible images
@@ -465,15 +464,16 @@ export default {
 
     // Make the image viewer visible
     showViewer: function(context) {
-        const wrap = context.container().select('.photoviewer')
-            .classed('hide', false);
-
+        const wrap = context.container().select('.photoviewer');
         const isHidden = wrap.selectAll('.photo-wrapper.mly-wrapper.hide').size();
 
         if (isHidden && _mlyViewer) {
-            wrap
-                .selectAll('.photo-wrapper:not(.mly-wrapper)')
-                .classed('hide', true);
+            for (const service of Object.values(services)) {
+                if (service === this) continue;
+                if (typeof service.hideViewer === 'function') {
+                    service.hideViewer(context);
+                }
+            }
 
             wrap
                 .selectAll('.photo-wrapper.mly-wrapper')
@@ -483,6 +483,7 @@ export default {
         }
 
         _isViewerOpen = true;
+        wrap.classed('hide', false);
 
         return this;
     },

modules/services/panoramax.js
@@ -9,6 +9,8 @@ import { geoExtent, geoScaleToZoom } from '../geo';
 import { t, localizer } from '../core/localizer';
 import pannellumPhotoFrame from './pannellum_photo';
 import planePhotoFrame from './plane_photo';
+import { services } from './';
+
 
 const apiUrl = 'https://api.panoramax.xyz/';
 const tileUrl = apiUrl + 'api/map/{z}/{x}/{y}.mvt';
@@ -255,9 +257,6 @@ export default {
             mockSequences: { rtree: new RBush(), lineString: {} },
             requests: { loaded: {}, inflight: {} }
         };
-
-        _currentScene.currentImage = null;
-        _activeImage = null;
     },
 
     /**
@@ -685,19 +684,22 @@ export default {
      * @param {*} context
      */
     showViewer: function (context) {
-        let wrap = context.container().select('.photoviewer')
-            .classed('hide', false);
+        let wrap = context.container().select('.photoviewer');
         let isHidden = wrap.selectAll('.photo-wrapper.panoramax-wrapper.hide').size();
         if (isHidden) {
-            wrap
-                .selectAll('.photo-wrapper:not(.panoramax-wrapper)')
-                .classed('hide', true);
+            for (const service of Object.values(services)) {
+                if (service === this) continue;
+                if (typeof service.hideViewer === 'function') {
+                    service.hideViewer(context);
+                }
+            }
             wrap
                 .selectAll('.photo-wrapper.panoramax-wrapper')
                 .classed('hide', false);
         }
 
         _isViewerOpen = true;
+        wrap.classed('hide', false);
 
         return this;
     },

modules/services/streetside.js
@@ -15,6 +15,8 @@ import {
 
 import { utilAesDecrypt, utilArrayUnion, utilQsString, utilRebind, utilStringQs, utilTiler, utilUniqueDomId } from '../util';
 
+import { services } from './';
+
 
 const streetsideApi = 'https://dev.virtualearth.net/REST/v1/Imagery/MetaData/Streetside?mapArea={bbox}&key={key}&count={count}&uriScheme=https';
 const maxResults = 500;
@@ -644,22 +646,22 @@ export default {
    * showViewer()
    */
   showViewer: function(context) {
-
-    let wrap = context.container().select('.photoviewer')
-      .classed('hide', false);
-
+    let wrap = context.container().select('.photoviewer');
     let isHidden = wrap.selectAll('.photo-wrapper.ms-wrapper.hide').size();
 
     if (isHidden) {
-      wrap
-        .selectAll('.photo-wrapper:not(.ms-wrapper)')
-        .classed('hide', true);
-
+      for (const service of Object.values(services)) {
+        if (service === this) continue;
+        if (typeof service.hideViewer === 'function') {
+          service.hideViewer(context);
+        }
+      }
       wrap
         .selectAll('.photo-wrapper.ms-wrapper')
         .classed('hide', false);
     }
 
+    wrap.classed('hide', false);
     return this;
   },
 

modules/services/vegbilder.js
@@ -8,6 +8,7 @@ import { utilQsString, utilTiler, utilRebind, utilArrayUnion, utilStringQs} from
 import {geoExtent, geoScaleToZoom, geoVecAngle, geoVecEqual} from '../geo';
 import pannellumPhotoFrame from './pannellum_photo';
 import planePhotoFrame from './plane_photo';
+import { services } from './';
 
 
 const owsEndpoint = 'https://www.vegvesen.no/kart/ogc/vegbilder_1_0/ows?';
@@ -493,20 +494,21 @@ export default {
   },
 
   showViewer: function (context) {
-    const viewer = context.container().select('.photoviewer')
-      .classed('hide', false);
-
+    const viewer = context.container().select('.photoviewer');
     const isHidden = viewer.selectAll('.photo-wrapper.vegbilder-wrapper.hide').size();
 
     if (isHidden) {
-      viewer
-        .selectAll('.photo-wrapper:not(.vegbilder-wrapper)')
-        .classed('hide', true);
-
+      for (const service of Object.values(services)) {
+        if (service === this) continue;
+        if (typeof service.hideViewer === 'function') {
+          service.hideViewer(context);
+        }
+      }
       viewer
         .selectAll('.photo-wrapper.vegbilder-wrapper')
         .classed('hide', false);
     }
+    viewer.classed('hide', false);
     return this;
   },
 

test/spec/services/kartaview.js
@@ -43,13 +43,12 @@ describe('iD.serviceKartaview', function() {
     });
 
     describe('#reset', function() {
-        it('resets cache and image', function() {
+        it('resets cache', function() {
             kartaview.cache().foo = 'bar';
             kartaview.selectImage(context, {key: 'baz'});
 
             kartaview.reset();
             expect(kartaview.cache()).to.not.have.property('foo');
-            expect(kartaview.getSelectedImage()).to.be.null;
         });
     });
 

test/spec/services/mapillary.js
@@ -42,13 +42,12 @@ describe('iD.serviceMapillary', function() {
 
 
     describe('#reset', function() {
-        it('resets cache and image', function() {
+        it('resets cache', function() {
             mapillary.cache().foo = 'bar';
             mapillary.selectImage(context, { key: 'baz', loc: [10,0] });
 
             mapillary.reset();
             expect(mapillary.cache()).to.not.have.property('foo');
-            expect(mapillary.getActiveImage()).to.be.null;
         });
     });
 

commit 81d94a138bd66e73356b5fe530d6b68c7617dbad

fix mapillary's panorama viewer from loading properly
this was a regression from the previous commit (19957d0e3)

also, harmonize code slightly between different services

modules/services/kartaview.js
@@ -367,8 +367,8 @@ export default {
 
 
     showViewer: function(context) {
-        var viewer = context.container().select('.photoviewer');
-        var isHidden = viewer.selectAll('.photo-wrapper.kartaview-wrapper.hide').size();
+        const wrap = context.container().select('.photoviewer');
+        const isHidden = wrap.selectAll('.photo-wrapper.kartaview-wrapper.hide').size();
 
         if (isHidden) {
             for (const service of Object.values(services)) {
@@ -377,12 +377,11 @@ export default {
                     service.hideViewer(context);
                 }
             }
-            viewer
+            wrap.classed('hide', false)
                 .selectAll('.photo-wrapper.kartaview-wrapper')
                 .classed('hide', false);
         }
 
-        viewer.classed('hide', false);
         return this;
     },
 

modules/services/mapilio.js
@@ -574,8 +574,8 @@ export default {
     },
 
     showViewer:function (context) {
-        let wrap = context.container().select('.photoviewer');
-        let isHidden = wrap.selectAll('.photo-wrapper.mapilio-wrapper.hide').size();
+        const wrap = context.container().select('.photoviewer');
+        const isHidden = wrap.selectAll('.photo-wrapper.mapilio-wrapper.hide').size();
 
         if (isHidden) {
             for (const service of Object.values(services)) {
@@ -584,12 +584,11 @@ export default {
                     service.hideViewer(context);
                 }
             }
-            wrap
+            wrap.classed('hide', false)
                 .selectAll('.photo-wrapper.mapilio-wrapper')
                 .classed('hide', false);
         }
 
-        wrap.classed('hide', false);
         return this;
     },
 
@@ -611,7 +610,6 @@ export default {
             .classed('currentView', false);
 
         this.setActiveImage();
-
         return this.setStyles(context, null);
     },
 

modules/services/mapillary.js
@@ -261,6 +261,8 @@ export default {
             sequences: { rtree: new RBush(), lineString: {} },
             requests: { loaded: {}, inflight: {} }
         };
+
+        _mlyActiveImage = null;
     },
 
     // Get visible images
@@ -475,7 +477,7 @@ export default {
                 }
             }
 
-            wrap
+            wrap.classed('hide', false)
                 .selectAll('.photo-wrapper.mly-wrapper')
                 .classed('hide', false);
 
@@ -483,8 +485,6 @@ export default {
         }
 
         _isViewerOpen = true;
-        wrap.classed('hide', false);
-
         return this;
     },
 

modules/services/panoramax.js
@@ -684,8 +684,8 @@ export default {
      * @param {*} context
      */
     showViewer: function (context) {
-        let wrap = context.container().select('.photoviewer');
-        let isHidden = wrap.selectAll('.photo-wrapper.panoramax-wrapper.hide').size();
+        const wrap = context.container().select('.photoviewer');
+        const isHidden = wrap.selectAll('.photo-wrapper.panoramax-wrapper.hide').size();
         if (isHidden) {
             for (const service of Object.values(services)) {
                 if (service === this) continue;
@@ -693,14 +693,12 @@ export default {
                     service.hideViewer(context);
                 }
             }
-            wrap
+            wrap.classed('hide', false)
                 .selectAll('.photo-wrapper.panoramax-wrapper')
                 .classed('hide', false);
         }
 
         _isViewerOpen = true;
-        wrap.classed('hide', false);
-
         return this;
     },
 

modules/services/streetside.js
@@ -646,8 +646,8 @@ export default {
    * showViewer()
    */
   showViewer: function(context) {
-    let wrap = context.container().select('.photoviewer');
-    let isHidden = wrap.selectAll('.photo-wrapper.ms-wrapper.hide').size();
+    const wrap = context.container().select('.photoviewer');
+    const isHidden = wrap.selectAll('.photo-wrapper.ms-wrapper.hide').size();
 
     if (isHidden) {
       for (const service of Object.values(services)) {
@@ -657,11 +657,11 @@ export default {
         }
       }
       wrap
+        .classed('hide', false)
         .selectAll('.photo-wrapper.ms-wrapper')
         .classed('hide', false);
     }
 
-    wrap.classed('hide', false);
     return this;
   },
 

modules/services/vegbilder.js
@@ -505,10 +505,10 @@ export default {
         }
       }
       viewer
+        .classed('hide', false)
         .selectAll('.photo-wrapper.vegbilder-wrapper')
         .classed('hide', false);
     }
-    viewer.classed('hide', false);
     return this;
   },
 